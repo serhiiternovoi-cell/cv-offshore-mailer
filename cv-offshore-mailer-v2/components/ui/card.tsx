2f7fecabcbe6ec0f3bfc7840c4ad8bf76ae7cc4b
serhiiternovoi-cell <serhiiternovoi@gmail.com>
2025-10-06 02:04:23 +0200

    Update card.tsx

--- cv-offshore-mailer-v2/components/ui/card.tsx
+++ cv-offshore-mailer-v2/components/ui/card.tsx
@@ -1,6 +1,8 @@
-import * as React from "react";
+import * as React from "react"
 
-interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string
+}
 
 export function Card({ className = "", ...props }: CardProps) {
   return (
@@ -8,27 +10,17 @@ export function Card({ className = "", ...props }: CardProps) {
       {...props}
       className={⁠ rounded-2xl border border-neutral-200 bg-white shadow-soft ${className} ⁠}
     />
-  );
+  )
 }
 
 export function CardHeader({ className = "", ...props }: CardProps) {
-  return <div {...props} className={⁠ p-6 ${className} ⁠} />;
+  return <div {...props} className={⁠ p-6 ${className} ⁠} />
 }
 
 export function CardContent({ className = "", ...props }: CardProps) {
-  return <div {...props} className={⁠ px-6 pb-6 ${className} ⁠} />;
+  return <div {...props} className={⁠ px-6 pb-6 ${className} ⁠} />
 }
 
 export function CardFooter({ className = "", ...props }: CardProps) {
-  return <div {...props} className={⁠ px-6 pb-6 ${className} ⁠} />;
-}
-
-export function CardTitle({ className = "", ...props }: CardProps) {
-  return <h3 {...props} className={⁠ text-lg font-semibold ${className} ⁠} />;
-}
-
-export function CardDescription({ className = "", ...props }: CardProps) {
-  return (
-    <p {...props} className={⁠ text-sm text-neutral-500 ${className} ⁠} />
-  );
+  return <div {...props} className={⁠ px-6 pb-6 ${className} ⁠} />
 }
