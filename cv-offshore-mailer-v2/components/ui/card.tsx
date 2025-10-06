690f05a8642cf2f48845d9650a8290034ee606ce
serhiiternovoi-cell <serhiiternovoi@gmail.com>
2025-10-06 02:18:01 +0200

    Update card.tsx

--- cv-offshore-mailer-v2/components/ui/card.tsx
+++ cv-offshore-mailer-v2/components/ui/card.tsx
@@ -1,7 +1,7 @@
-import * as React from "react"
+import * as React from "react";
 
 interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
-  className?: string
+  className?: string;
 }
 
 export function Card({ className = "", ...props }: CardProps) {
@@ -9,18 +9,20 @@ export function Card({ className = "", ...props }: CardProps) {
     <div
       {...props}
       className={⁠ rounded-2xl border border-neutral-200 bg-white shadow-soft ${className} ⁠}
-    />
-  )
+    >
+      {props.children}
+    </div>
+  );
 }
 
 export function CardHeader({ className = "", ...props }: CardProps) {
-  return <div {...props} className={⁠ p-6 ${className} ⁠} />
+  return <div {...props} className={⁠ p-6 ${className} ⁠} />;
 }
 
 export function CardContent({ className = "", ...props }: CardProps) {
-  return <div {...props} className={⁠ px-6 pb-6 ${className} ⁠} />
+  return <div {...props} className={⁠ px-6 pb-6 ${className} ⁠} />;
 }
 
 export function CardFooter({ className = "", ...props }: CardProps) {
-  return <div {...props} className={⁠ px-6 pb-6 ${className} ⁠} />
+  return <div {...props} className={⁠ px-6 pb-6 ${className} ⁠} />;
 }
